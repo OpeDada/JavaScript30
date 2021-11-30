ae2efb706ae7bae0574b0ef6fba21f7211dd80c3
OpeDada <opedada@gmail.com>
2021-11-30 12:13:08 +0900

    activate the buttons

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,10 +1,13 @@
 let countdown;
 const timerDisplay = document.querySelector(".display__time-left");
+const endTime = document.querySelector(".display__end-time");
+const buttons = document.querySelectorAll("[data-time]");
 
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -30,4 +33,16 @@ function displayTimeLeft(seconds) {
 
 function displayEndTime(timestamp) {
   const end = new Date(timestamp);
+  const hour = end.getHours();
+  const adjustedHour = hour > 12 ? hour - 12 : hour;
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back At ${adjustedHour}:${
+    minutes < 10 ? "0" : ""
+  }${minutes}`;
 }
+
+function startTimer() {
+  console.log(this);
+}
+
+buttons.forEach((button) => button.addEventListener("click", startTimer));
