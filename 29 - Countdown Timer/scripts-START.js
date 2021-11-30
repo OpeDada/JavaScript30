2b226e6ff21eea8aa55c47e2d57f21c66b40207a
OpeDada <opedada@gmail.com>
2021-11-30 11:05:23 +0900

    get minutes and remaining seconds

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -3,6 +3,7 @@ let countdown;
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -12,5 +13,11 @@ function timer(seconds) {
       return;
     }
     // display it
+    displayTimeLeft(secondsLeft);
   }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
 }
